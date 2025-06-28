6f376a5a9cc67e18d5f424c6928f8bf82dee8a1e
Moid Abdul <153217551+Morizz00@users.noreply.github.com>
2025-06-28 17:05:27 +0530

    redih

--- storage/redis.go
+++ storage/redis.go
@@ -26,12 +26,35 @@ func StoreFile(key string, file StoredFile) error {
 	return rdb.Set(ctx, key, u, time.Minute*5).Err()
 }
 
-func GetAndDelete(key string) (StoredFile, error) {
-	val, err := rdb.GetDel(ctx, key).Bytes()
+//	func GetAndDelete(key string) (StoredFile, error) {
+//		val, err := rdb.GetDel(ctx, key).Bytes()
+//		if err != nil {
+//			return StoredFile{}, err
+//		}
+//		var res StoredFile
+//		err = json.Unmarshal(val, &res)
+//		return res, err
+//	}
+func Get(key string) (StoredFile, error) {
+	val, err := rdb.Get(ctx, key).Bytes()
 	if err != nil {
 		return StoredFile{}, err
 	}
 	var res StoredFile
 	err = json.Unmarshal(val, &res)
 	return res, err
+}
+func Delete(key string) error {
+	return rdb.Del(ctx, key).Err()
+}
+func UpdateFilePreservingTTL(key string, file StoredFile) error {
+	u, err := json.Marshal(file)
+	if err != nil {
+		return err
+	}
+	ttl, err := rdb.TTL(ctx, key).Result()
+	if err != nil || ttl <= 0 {
+		ttl = time.Minute * 5
+	}
+	return rdb.Set(ctx, key, u, ttl).Err()
 }
