3e61a33f7d69a06e79282a7cae834010eedb88de
Moid Abdul <153217551+Morizz00@users.noreply.github.com>
2025-06-28 17:02:55 +0530

    adding the download limiter

--- handlers/upload.go
+++ handlers/upload.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/Morizz00/self-destruct-share-api/storage"
 	"github.com/Morizz00/self-destruct-share-api/utils"
@@ -14,7 +15,7 @@ type StoredFile struct {
 	MIME          string `json:"mime"`
 	Data          []byte `json:"date"`
 	Password      string `json:"password"`
-	DownloadsLeft int    `json:"downloads left"`
+	DownloadsLeft int    `json:"downloads_left"`
 }
 
 func Upload(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +25,10 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	password := r.FormValue("password")
+	downloads := 1
+	if parsed, err := strconv.Atoi(r.FormValue("downloads")); err == nil && parsed > 0 {
+		downloads = parsed
+	}
 
 	defer file.Close()
 	fileData, err := io.ReadAll(file)
@@ -33,13 +38,15 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	storeIt := storage.StoredFile{
-		FileName: fileHeader.Filename,
-		MIME:     fileHeader.Header.Get("Content-Type"),
-		Data:     fileData,
-		Password: password,
+		FileName:      fileHeader.Filename,
+		MIME:          fileHeader.Header.Get("Content-Type"),
+		Data:          fileData,
+		Password:      password,
+		DownloadsLeft: downloads,
 	}
 	id := utils.GenerateID()
-	err = storage.StoreFile(id, storage.StoredFile(storeIt))
+	err = storage.StoreFile(id, storeIt)
+
 	if err != nil {
 		http.Error(w, "storage error", http.StatusInternalServerError)
 		return
@@ -47,4 +54,3 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 	dowloadURL := fmt.Sprintf("/file/%s", id)
 	fmt.Fprintf(w, "File uploaded--Download:/file/%s\n", dowloadURL)
 }
-
