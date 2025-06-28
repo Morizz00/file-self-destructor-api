22af111a4a66b9352eb50d75121ac97d63324c37
Moid Abdul <153217551+Morizz00@users.noreply.github.com>
2025-06-28 17:04:27 +0530

    appendage to the download limiter

--- handlers/download.go
+++ handlers/download.go
@@ -9,7 +9,7 @@ import (
 
 func DownloadFile(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
-	storedData, err := storage.GetAndDelete(id)
+	storedData, err := storage.Get(id)
 	if err != nil {
 		http.Error(w, "file expired or not foundszies", http.StatusNotFound)
 		return
@@ -19,6 +19,24 @@ func DownloadFile(w http.ResponseWriter, r *http.Request) {
 	if storedData.Password != "" && storedData.Password != password {
 		http.Error(w, "wrong or missing password lil bud", http.StatusForbidden)
 		return
+	}
+	if storedData.DownloadsLeft <= 0 {
+		http.Error(w, "No downloads left lil bro", http.StatusGone)
+		return
+	}
+	if storedData.DownloadsLeft == 1 {
+		err := storage.Delete(id)
+		if err != nil {
+			http.Error(w, "Failed to self-destruct lil bro", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		storedData.DownloadsLeft--
+		err := storage.UpdateFilePreservingTTL(id, storedData)
+		if err != nil {
+			http.Error(w, "Failed to update download count", http.StatusInternalServerError)
+			return
+		}
 	}
 	w.Header().Set("Content-Disposition", "attachment; filename="+storedData.FileName)
 	w.Header().Set("Content-Type", storedData.MIME)
