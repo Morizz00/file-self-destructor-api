95c6993bf36f9e7d45e53dfe3168c455592dded8
Moid Abdul <153217551+Morizz00@users.noreply.github.com>
2025-06-29 20:54:09 +0530

    minor change,still redis goated tho

--- storage/redis.go
+++ storage/redis.go
@@ -25,16 +25,6 @@ func StoreFile(key string, file StoredFile) error {
 	}
 	return rdb.Set(ctx, key, u, time.Minute*5).Err()
 }
-
-//	func GetAndDelete(key string) (StoredFile, error) {
-//		val, err := rdb.GetDel(ctx, key).Bytes()
-//		if err != nil {
-//			return StoredFile{}, err
-//		}
-//		var res StoredFile
-//		err = json.Unmarshal(val, &res)
-//		return res, err
-//	}
 func Get(key string) (StoredFile, error) {
 	val, err := rdb.Get(ctx, key).Bytes()
 	if err != nil {
